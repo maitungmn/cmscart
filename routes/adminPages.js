38b3e1934613be184e18d86836e2ff2563e78be9
MaiTung <goingdownfr@gmail.com>
2018-03-31 21:25:49 +0700

    Video 12: Post Add Page 2! (Error at Slug does not auto generated)

--- routes/adminPages.js
+++ routes/adminPages.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /**
+ * Get Page Model
+ */
+var Page = require('../models/pages');
+
+/**
  * Get Pages Index
  */
 router.get("/", function (req, res) {
@@ -34,12 +39,35 @@ router.post("/add-page", function (req, res) {
     var errors = req.validationErrors();
     if(errors){
         res.render('admin/add_page',{
+            errors: errors,
             title: title,
             slug: slug,
             content: content
         })
     }else{
-        console.log('success!');
+        Page.findOne({slug: slug}, function (err, page) {
+            if(page){
+                req.flash('danger', 'Page slug exists, choose another!');
+                res.render('admin/add_page',{
+                    title: title,
+                    slug: slug,
+                    content: content
+                })
+            }else {
+                var page = new Page({
+                    title: title,
+                    slug: slug,
+                    content: content,
+                    sorting: 0
+                });
+                page.save(function (err) {
+                    if(err)
+                        return console.log(err);
+                    req.flash('success', 'Page added!');
+                    res.redirect('/admin/pages');
+                })
+            }
+        })
     }
 
 });
