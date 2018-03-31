090293087096121a4a75bad15ea31c77dd96a738
MaiTung <goingdownfr@gmail.com>
2018-03-31 13:21:56 +0700

    ShoppingCart

---
+++ app.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var path = require('path');
+var mongoose = require('mongoose');
+var config = require('./config/database');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var expressValidator = require('express-validator');
+
+
+//Connect to DB
+mongoose.connect(config.database);
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+    console.log('Connected MongoDB');
+});
+
+// Init App
+var app = express();
+
+//View Engine Setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Set Router
+var pages = require('./routes/pages.js');
+var adminPages = require('./routes/adminPages.js');
+
+//Body-parser MiddleWare
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+//Express Sesison MiddleWare
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
+
+//Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.');
+        var root = namespace.shift();
+        var formParam = root;
+        while (namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
+
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+
+
+app.use('/', pages);
+app.use('/admin/pages', adminPages);
+
+//Start the server
+var port = 3000;
+app.listen(port, function () {
+    console.log("Server is Listening on Port: " + port)
+});
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    database: 'mongodb://root:root@ds229909.mlab.com:29909/cmscart'
+};
