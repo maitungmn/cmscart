962a637a86ca6ed236f71a824e4502d06eb1c985
MaiTung <goingdownfr@gmail.com>
2018-04-05 12:59:48 +0700

    Video 45: Clear Cart Done

--- routes/cart.js
+++ routes/cart.js
@@ -7,11 +7,11 @@ var Product = require('../models/product');
 /*
  * GET add product to cart
  */
-router.get('/', function (req, res) {
+router.get('/add/:product', function (req, res) {
 
     var slug = req.params.product;
 
-    Page.findOne({slug: slug}, function (err, p) {
+    Product.findOne({slug: slug}, function (err, p) {
         if (err)
             console.log(err);
 
@@ -38,16 +38,89 @@ router.get('/', function (req, res) {
                 cart.push({
                     title: slug,
                     qty: 1,
-                    price: parseFloat(p.price)
+                    price: parseFloat(p.price).toFixed(2),
+                    image: '/product_images/' + p._id + '/' + p.image
                 })
             }
         }
 
+        // console.log(req.session.cart);
+        req.flash('success', 'Product added!');
+        res.redirect('back');
 
     });
 
 });
 
+/*
+ * GET checkout Page
+ */
+router.get('/checkout', function (req, res) {
+    if(req.session.cart && req.session.cart.length == 0){
+        delete req.session.cart;
+        res.redirect('/products/cart/checkout');
+    } else {
+        res.render('checkout', {
+            title: 'Checkout',
+            cart: req.session.cart
+        })
+    }
+});
+
+/*
+ * GET update Product
+ */
+router.get('/update/:product', function (req, res) {
+    var slug = req.params.product;
+    var cart = req.session.cart;
+    var action = req.query.action;
+
+    for (var i = 0; i < cart.length; i++){
+        if (cart[i].title == slug){
+            switch (action) {
+                case "add":
+                    cart[i].qty++;
+                    break;
+                case "remove":
+                    cart [i].qty--;
+                    if (cart[i].qty < 1) cart.splice(i, 1);
+                    break;
+                case "clear":
+                    cart.splice(i, 1);
+                    if (cart.length == 0) delete req.sessionl.cart;
+                    break;
+                default:
+                    console.log('Update problem');
+                    break;
+            }
+            break;
+        }
+    }
+    req.flash('success', 'Cart updated!');
+    res.redirect('/products/cart/checkout');
+});
+
+/*
+ * GET clear cart
+ */
+router.get('/clear', function (req, res) {
+
+    delete req.session.cart;
+
+    req.flash('success', 'Cart cleared!');
+    res.redirect('/products/cart/checkout');
+});
+
+/*
+ * GET buy now
+ */
+router.get('/buynow', function (req, res) {
+
+    delete req.session.cart;
+
+    res.sendStatus(200);
+
+});
 
 // Exports
 module.exports = router;
